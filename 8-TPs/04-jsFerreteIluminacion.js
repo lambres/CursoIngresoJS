1d271554bca26043532aff5d4b35ef3902aec94b
Ruben Zevallos <rzevallos@everest.com.ar>
2022-01-31 15:31:19 -0300

    Sin terminar

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -2,13 +2,63 @@
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca 
+el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un 
+descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto 
+con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
 {
- 	
+    //declaro variable para capturar los valores del formulario html
+    let CantLamparas;
+    let marca;
+    let descuento;
+    let neto;
+    let bruto;
+    let impuesto;
+    const precio = 35;
+    descuento = 0
+    CantLamparas = parseInt(document.getElementById("txtIdCantidad").value);
+    marca = document.getElementById("txtIdprecioDescuento").value;
+
+    switch (CantLamparas){
+        case 6:
+            descuento = 50;
+            break;
+        case 5:
+            if (marca=="ArgentinaLuz"){
+                descuento = 40;
+            }else{
+                descuento=30;
+            }
+            break;
+        case 4:
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+                descuento = 25;
+            }else{
+                descuento = 20;
+            }
+            break;
+        case 3:
+            if (marca == "ArgentinaLuz"){
+                descuento = 15;
+            }else if (marca == "FelipeLamparas" ){
+                descuento = 10;
+            }else{
+                descuento = 5;
+            }
+            break;
+    }
+    neto = CantLamparas * precio - CantLamparas * precio * descuento / 100;
+    if (neto >= 120){
+        impuesto = neto * 10 / 100;
+        bruto = neto + impuesto;
+        document.getElementById("txtIdprecioDescuento").value = `Usted pago ${impuesto} de IIBB., total a abonar ${bruto.toFixed(2)}`;
+    }else{
+        document.getElementById("txtIdprecioDescuento").value = neto.toFixed(2);
+    }
 }
