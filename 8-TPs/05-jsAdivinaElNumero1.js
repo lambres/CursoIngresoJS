10cca51acaa34168d331371e3fe9068a42830c86
Ruben Zevallos <rzevallos@everest.com.ar>
2022-02-02 14:45:55 -0300

    TP 5

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -5,7 +5,8 @@ secreto del 1 al 100, en la pantalla del juego
   ingresado es el mismo que el número secreto se dará 
   por terminado el juego con un mensaje similar a este: 
 “Usted es un ganador!!! y en solo X intentos”.
-de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
+de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”
+del número secreto.
 */
 var numeroSecreto; 
 var contadorIntentos;
@@ -13,14 +14,27 @@ var contadorIntentos;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+  let maximo = 100;
+	let minimo= 1;
+
+	numeroSecreto = Math.round((Math.random() * (maximo - minimo) + minimo)); 
 		//alert(numeroSecreto );
-	
+	contadorIntentos=0
 
 }
 
 function verificar()
 {
-	
+	let nroIngreado;
+  nroIngreado = parseInt(document.getElementById("txtIdNumero").value);
+  console.debug ("nroIngresado " + nroIngreado + " / numeroSecreto " + numeroSecreto);
+  if (nroIngreado < numeroSecreto){
+      document.getElementById("txtIdIntentos").value = "Falta ....";
+  }else if(nroIngreado==numeroSecreto){
+      document.getElementById("txtIdIntentos").value = "Usted es un ganador!!! y en solo " + contadorIntentos + " sintentos";
+  }else{
+      document.getElementById("txtIdIntentos").value = "se pasó…";
+  }
+  contadorIntentos += 1;
 	
 }
