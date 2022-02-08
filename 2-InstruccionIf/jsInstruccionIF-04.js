513a6bf6df989e3695c35454119af85f02e2c359
lambres <lambres@gmail.com>
2022-02-07 22:33:20 -0300

    Agregado nombre y division

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -5,16 +5,15 @@ División K
 Ejercicio 4 JS
 */
 {
-//Defino varfiable
-let edad;
-//tomo la edad  
-edad = parseInt(document.getElementById("txtIdEdad").value);
-//muestro el texto Eres mayor de edad si se cumple la condicion > 18 años
-if (edad >= 13 && edad <= 17)
-{
-	alert("Eres Adolescente");
-}else{
-	alert("No eres Adolescente");
-}
+	//Defino varfiable
+	let edad;
+	//tomo la edad  
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	//muestro el texto Eres mayor de edad si se cumple la condicion > 18 años
+	if (edad >= 13 && edad <= 17) {
+		alert("Eres Adolescente");
+	} else {
+		alert("No eres Adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
